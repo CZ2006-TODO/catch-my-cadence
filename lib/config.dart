d2a2fe3f660bb2a2cac0df0ddda192afdfb621dd
prolude1 <79588462+prolude1@users.noreply.github.com>
2021-09-05 15:43:21 +0800

    Revert "Revert "Merge branch 'main' of https://github.com/tohhongxiang123/CZ2006 into main""
    This reverts commit 66ecf938ad65770398c79514c50bb06c8a0a14bb.

---
+++ lib/config.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+// Config stores the general global variables
+class Config {
+  // Private
+  static const String _tokenFileName = "usrToken";
+
+  // Public
+  static late String clientId;
+  static late String redirectUrl;
+
+  // Returns the path to the stored auth token file.
+  static Future<File> get tokenFilePath async {
+    final appDirectory = await getApplicationDocumentsDirectory();
+    return File("${appDirectory.path}/$_tokenFileName");
+  }
+}
