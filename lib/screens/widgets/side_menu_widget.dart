5c164970a4a885ee84ab8b2246ba52bf742dce1f
Lim-Shi-Han <79437795+Lim-Shi-Han@users.noreply.github.com>
2021-09-30 14:25:21 +0800

    Made the above changes
    Hi can help to check if there is a hamburger button at the main screen? Also, can help to see if the side menu is mostly opaque? It's supposed to be 0.8 opacity... Thanks:)

--- lib/screens/widgets/side_menu_widget.dart
+++ lib/screens/widgets/side_menu_widget.dart
@@ -10,55 +10,52 @@ class SideMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Theme(
-      data: Theme.of(context).copyWith(canvasColor: Color(0x3E3E3E)),
-      child: ListView(padding: EdgeInsets.zero, children: <Widget>[
-        _buildDrawerHeader(context),
-        _buildCMCItem(context),
-        Divider(),
-        _buildAHItem(context),
-        Divider(),
-        _buildInfoItem(context),
-        Divider(),
-        _buildHelpItem(context),
-        Divider(),
-        _buildSettingsItem(context),
-      ]),
-    );
+        data: Theme.of(context)
+            .copyWith(canvasColor: Colors.black.withOpacity(0.8)),
+        child: Drawer(
+          child: ListView(padding: EdgeInsets.zero, children: <Widget>[
+            _buildDrawerHeader(context),
+            _buildCMCItem(context),
+            Divider(),
+            _buildAHItem(context),
+            Divider(),
+            _buildInfoItem(context),
+            Divider(),
+            _buildHelpItem(context),
+            Divider(),
+            _buildSettingsItem(context),
+          ]),
+        ));
   }
 
   UserAccountsDrawerHeader _buildDrawerHeader(BuildContext context) {
     return UserAccountsDrawerHeader(
         accountName: Text(
           'Tan Ah Kow',
-          style: TextStyle(backgroundColor: Color(0xFF778899)),
+          style: TextStyle(backgroundColor: Color(0xFF778899).withOpacity(0.8)),
         ),
         accountEmail: Text(
           'tanahkow@gmail.com',
-          style: TextStyle(backgroundColor: Color(0xFF778899)),
+          style: TextStyle(backgroundColor: Color(0xFF778899).withOpacity(0.8)),
         ),
         currentAccountPicture: CircleAvatar(
           backgroundColor: Colors.transparent,
           child: Image.asset("assets/images/splash_screen.png"),
         ),
         decoration: BoxDecoration(
-          color: Color(0xFF778899),
+          color: Color(0xFF778899).withOpacity(0.8),
         ));
   }
 
   ListTile _buildCMCItem(BuildContext context) {
     return ListTile(
         title: Text(
-          'Catch My Cadence',
+          'Home',
           style: TextStyle(color: Colors.white),
         ),
-        leading: ConstrainedBox(
-          constraints: BoxConstraints(
-            minWidth: 44,
-            minHeight: 44,
-            maxWidth: 44,
-            maxHeight: 44,
-          ),
-          child: Image.asset("assets/images/cmc_home.png", fit: BoxFit.cover),
+        leading: Icon(
+          Icons.home,
+          color: Colors.white,
         ),
         onTap: () {
           Navigator.of(context).pop();
@@ -74,14 +71,9 @@ class SideMenu extends StatelessWidget {
         'Activity History',
         style: TextStyle(color: Colors.white),
       ),
-      leading: ConstrainedBox(
-        constraints: BoxConstraints(
-          minWidth: 44,
-          minHeight: 44,
-          maxWidth: 44,
-          maxHeight: 44,
-        ),
-        child: Image.asset("assets/images/ah.png", fit: BoxFit.cover),
+      leading: Icon(
+        Icons.history,
+        color: Colors.white,
       ),
       // onTap:(){
       // TODO: Link to relevant screens
@@ -95,14 +87,9 @@ class SideMenu extends StatelessWidget {
         'About this App',
         style: TextStyle(color: Colors.white),
       ),
-      leading: ConstrainedBox(
-        constraints: BoxConstraints(
-          minWidth: 44,
-          minHeight: 44,
-          maxWidth: 44,
-          maxHeight: 44,
-        ),
-        child: Image.asset("assets/images/info.png", fit: BoxFit.cover),
+      leading: Icon(
+        Icons.info,
+        color: Colors.white,
       ),
       // onTap:(){
       // TODO: Link to relevant screens
@@ -116,14 +103,9 @@ class SideMenu extends StatelessWidget {
         'Help',
         style: TextStyle(color: Colors.white),
       ),
-      leading: ConstrainedBox(
-        constraints: BoxConstraints(
-          minWidth: 44,
-          minHeight: 44,
-          maxWidth: 44,
-          maxHeight: 44,
-        ),
-        child: Image.asset("assets/images/help.png", fit: BoxFit.cover),
+      leading: Icon(
+        Icons.help,
+        color: Colors.white,
       ),
       // onTap:(){
       // TODO: Link to relevant screens
@@ -137,14 +119,9 @@ class SideMenu extends StatelessWidget {
         'Settings',
         style: TextStyle(color: Colors.white),
       ),
-      leading: ConstrainedBox(
-        constraints: BoxConstraints(
-          minWidth: 44,
-          minHeight: 44,
-          maxWidth: 44,
-          maxHeight: 44,
-        ),
-        child: Image.asset("assets/images/settings.png", fit: BoxFit.cover),
+      leading: Icon(
+        Icons.settings,
+        color: Colors.white,
       ),
       // onTap:(){
       // TODO: Link to relevant screens
