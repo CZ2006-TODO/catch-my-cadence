11e9c57a82dcefa938208dc3b4763c4720ecbe83
Daryl <aka.hjd@gmail.com>
2021-10-01 00:34:41 +0800

    my eyes hurt.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 // App starts running from the main activity.
 void main() {
   runApp(MaterialApp(
+    theme: ThemeData.dark(),
     title: "Catch My Cadence",
     // App always shows the loading screen when open.
     initialRoute: RouteDelegator.LOADING_SCREEN_ROUTE,
