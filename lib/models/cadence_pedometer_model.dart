cf14c7284797114998f7182377fc676a112cd51a
Daryl <aka.hjd@gmail.com>
2021-10-04 12:11:43 +0800

    improve timer performance

--- lib/models/cadence_pedometer_model.dart
+++ lib/models/cadence_pedometer_model.dart
@@ -12,9 +12,9 @@ class CadencePedometerModel with ChangeNotifier {
   static const _queueSize = 5;
 
   // Data streams
-  ListQueue<int> _cadences = ListQueue(_queueSize); // Holds past calculated cadences.
+  ListQueue<int> _cadences = ListQueue(_queueSize); // Holds past cadences.
   late Stream<StepCount> _stepCountStream;
-  late Timer _timer;
+  late Timer? _timer;
 
   late int _numSteps; // Number of steps from start.
   late bool _isActive; // Whether cadence is being calculated
@@ -22,69 +22,88 @@ class CadencePedometerModel with ChangeNotifier {
 
   CadencePedometerModel() {
     // Initialise the starting state for the model.
-    _resetState();
-    _setUpDataStreams();
+    _setInactiveState();
+
+    // _timer not set up as initially, cadence is ot calculated.
+    _setUpCountStream();
   }
 
-  // resetState : Initialise the starting state for the model.
-  // This includes setting up the required attributes and streams,
-  // and also clears the cadence queue.
-  void _resetState() {
+  // _setUpCountStream : Sets up streams for pedometer data.
+  // https://www.all8.com/tools/bpm.htm
+  // Quite accurate for calculating cadence, trying to replicate.
+  void _setUpCountStream() {
+    // This step stream adds one to the current step count when
+    // calculation is active.
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen((StepCount event) {
+      if (_isActive) {
+        _numSteps += 1;
+        notifyListeners();
+      }
+    }).onError((e) {
+      log("Pedometer Error: ${e.toString()}");
+    });
+  }
+
+  // _setInactiveState : Put model into inactive state.
+  // This resets calculation variables to 0, stops the periodic timer,
+  // and empties the cadence queue.
+  void _setInactiveState() {
     _numSteps = _startTime = 0;
     _isActive = false;
+    _timer?.cancel();
     _cadences.clear();
   }
 
-  // setUpStepDataStreams : Sets up streams for the cadence data.
-  // https://www.all8.com/tools/bpm.htm
-  // Quite accurate for calculating cadence, trying to replicate.
-  void _setUpDataStreams() {
-    // This step stream adds one to the current step count when
-    // calculation is active.
-    _stepCountStream = Pedometer.stepCountStream
-      ..listen((StepCount event) {
-        if (_isActive) {
-          _numSteps += 1;
-          notifyListeners();
-        }
-      }).onError((e) {
-        log("Pedometer Error: ${e.toString()}");
-      });
+  // _setActiveState : Put model into active state.
+  // This sets the start time and also the timer for periodic calculation.
+  void _setActiveState() {
+    // Error correction with pedometer delay.
+    _startTime = DateTime.now().millisecondsSinceEpoch +
+        Duration(milliseconds: 500).inMilliseconds;
+    // Create a new periodic timer.
+    _setUpTimer();
+  }
 
+  // _setUpTimer : Creates a new periodic timer for cadence calculation.
+  // This is called when the model is put into active state.
+  // The timer is cancelled when the model is toggled to inactive state, and
+  // a new timer will be created when it is re-toggled to active state.
+  void _setUpTimer() {
     // This periodic timer updates the current cadence when calculation is active.
     _timer = Timer.periodic(Duration(seconds: 1), (timer) {
       if (_isActive) {
-        var timeDifference = DateTime.now().millisecondsSinceEpoch - _startTime;
+        int timeDifference = DateTime.now().millisecondsSinceEpoch - _startTime;
+        // Needed due to start time error correction.
         if (timeDifference <= 0) {
           return;
         }
-        var cadence = ((_numSteps / timeDifference) *
+        int cadence = ((_numSteps / timeDifference) *
                 Duration.secondsPerMinute *
                 Duration.millisecondsPerSecond)
             .round();
         _cadences.add(cadence);
+        // This ensures only _queueSize elements in the queue.
         if (_cadences.length > _queueSize) {
           _cadences.removeFirst();
         }
-        log(_cadences.toString());
-        log(this.cadence.toString());
+        log(cadence.toString());
         notifyListeners();
       }
     });
   }
 
-  // toggleStatus : Toggle isActive.
+  // toggleStatus : Toggles active state.
+  // The model will re-configure itself based on the new state.
   void toggleStatus() {
     _isActive = !_isActive;
     log("Setting active state to $_isActive");
     if (!_isActive) {
       log("Active state has been set to false, resetting...");
-      _resetState();
+      _setInactiveState();
     } else {
-      // Error correction with pedometer delay.
-      _startTime = DateTime.now().millisecondsSinceEpoch +
-          Duration(milliseconds: 500).inMilliseconds;
       log("Active state has been set to true, preparing for cadence calculation...");
+      _setActiveState();
     }
     notifyListeners();
   }
