44adec042db415a6e142e3405f210da961889699
Toh Hong Xiang <tohhongxiang@gmail.com>
2021-11-01 16:08:48 +0800

    chore: Remove unused imports

--- test/album_art_widget_test.dart
+++ test/album_art_widget_test.dart
@@ -1,4 +1,3 @@
-import 'package:catch_my_cadence/screens/loading_screen.dart';
 import 'package:catch_my_cadence/screens/widgets/album_art_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
